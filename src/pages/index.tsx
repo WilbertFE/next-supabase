e12664efecb9663a8ab20ed1a4a6a331a11d6880
WilbertFE <wilbertbernadi699@gmail.com>
2025-08-06 08:46:23 +0700

    feat: ui show menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import supabase from "@/lib/db";
 import type { IMenu } from "@/types/menu";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const Home = () => {
@@ -16,11 +19,36 @@ const Home = () => {
     fetchMenus();
   }, [supabase]);
 
-  console.log(menus);
-
   return (
-    <div>
-      <div>Home</div>
+    <div className="container mx-auto py-8">
+      <div className="text-3xl font-bold mb-4">Menu</div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {menus.map((menu) => (
+          <Card key={menu.id}>
+            <CardContent>
+              <Image
+                src={menu.image}
+                alt={menu.name}
+                width={200}
+                height={200}
+                className="w-full h-[30vh] object-cover rounded-lg"
+              />
+              <div className="flex justify-between mt-4">
+                <div>
+                  <h4 className="font-semibold text-xl">{menu.name}</h4>
+                  <p>{menu.category}</p>
+                </div>
+                <div className="font-semibold text-2xl">${menu.price}.00</div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full font-bold" size="lg">
+                Detail Menu
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
